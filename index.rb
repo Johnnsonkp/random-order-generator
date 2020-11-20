5715bbb285ae99187244880fc0e93d0e09f675e2
John Nkp <john.nkp1@gmail.com>
2020-11-20 11:12:29 +1100

    Wrote psuedocode in index.rb, added flowchart, formatted readme

---
+++ index.rb
@@ -0,0 +1,6 @@
+# INPUTS: Menu choice, name, options for quit etc
+# DATA: Array of names
+# OUTPUT: Size of class, Menu, names, prompts for quit
+# ADD PERSON IMPLEMENTATION: User input, add to array .push?
+# RANDOM ORDER IMPLEMENTATION: Copy array, select random name 
+#   get name at random index, delete name from array, delete name for array, keep going until array 
